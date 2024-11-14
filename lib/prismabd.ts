7004010856a029599ecde54a01b6aaee1a325e44
Younes <bettiouim77@gmail.com>
2024-11-14 14:28:47 +0100

    update

--- lib/prismabd.ts
+++ lib/prismabd.ts
@@ -1,12 +1,11 @@
-import { $Enums, PrismaClient } from "@prisma/client/edge";
+import { $Enums, PrismaClient } from "@prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
-
 const prismadb = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = prismadb;
 }
 export const animalsTypes = Object.values($Enums.Animal);
-export default prismadb;
+export default prismadb
