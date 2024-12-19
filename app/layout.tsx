1ea3dde71c6c041fb99b08106febb9aa0952f853
Younes <bettiouim77@gmail.com>
2024-12-19 20:28:14 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default async function RootLayout({
       >
         <body>
           <ThemeProvider
+
             attribute="class"
             defaultTheme="system"
             enableSystem
